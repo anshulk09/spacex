3ade48876c719b44fd2f8aaa3aa05374e9c44116
anshulk09 <anshul.it2008@gmail.com>
2020-11-01 22:17:59 +0530

    start change

--- scripts/startProd.js
+++ scripts/startProd.js
@@ -6,29 +6,30 @@ const cluster = require('cluster');
 
 const { app } = require('../build/server');
 
-const PORT = process.env.PORT || 5000;
-app.set('port', process.env.PORT || 5000);
+const PORT = process.env.PORT || 3000;
 
-// Use the native Node.js cluster module to create a worker processes for each CPU
-if (cluster.isMaster) {
-  console.log(`Master pid: ${process.pid}`);
-
-  const cpuCount = require('os').cpus().length;
-  for (let i = 0; i < cpuCount; i += 1) {
-    cluster.fork();
+app.listen(PORT, err => {
+  if (err) {
+    return console.error(err);
   }
 
-  cluster.on('exit', worker => {
-    console.log(`Worker ${worker.process.pid} died`);
-  });
-} else {
-  app.listen(PORT, '0.0.0.0', err => {
-    if (err) {
-      return console.error(err);
-    }
+  console.info(
+    `Server running on port ${PORT} -- Worker pid: ${cluster.worker.process.pid}`
+  );
+});
 
-    console.info(
-      `Server running on port ${PORT} -- Worker pid: ${cluster.worker.process.pid}`
-    );
-  });
-}
+// Use the native Node.js cluster module to create a worker processes for each CPU
+// if (cluster.isMaster) {
+//   console.log(`Master pid: ${process.pid}`);
+
+//   const cpuCount = require('os').cpus().length;
+//   for (let i = 0; i < cpuCount; i += 1) {
+//     cluster.fork();
+//   }
+
+//   cluster.on('exit', worker => {
+//     console.log(`Worker ${worker.process.pid} died`);
+//   });
+// } else {
+
+// }
