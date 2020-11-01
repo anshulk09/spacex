7206d5e3108188264f24ce72a7a28aea7aa20699
anshulk09 <anshul.it2008@gmail.com>
2020-11-01 21:51:54 +0530

    port change

--- scripts/start.js
+++ scripts/start.js
@@ -57,7 +57,7 @@ choosePort(HOST, DEFAULT_PORT).then(port => {
 
   const urls = prepareUrls('http', HOST, port);
 
-  server.listen(port, HOST, err => {
+  server.listen(5000, '0.0.0.0', err => {
     if (err) {
       return console.log(err);
     }
